c2ff2000e05dce13215753b0f3976511fc08faaf
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-07 22:46:41 +0700

    add news routes

--- src/routes/private/index.js
+++ src/routes/private/index.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
 const user = require("../../controllers/user");
+const news = require("../../controllers/news");
 
 router.get("/user", user.getUser);
 router.patch("/user", user.patchUser);
 router.put("/user", user.putUser);
 // router.delete("/user", user.deleteUser);
+
+router.get("/news", news.getNews);
+router.post("/news", news.postNews);
+router.patch("/news/:id", news.patchNews);
+router.put("/news/:id", news.getNews);
+router.delete("/news/:id", news.deleteNews);
 
 module.exports = router;
